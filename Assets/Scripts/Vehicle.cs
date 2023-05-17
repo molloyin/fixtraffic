a559f20179b9cb5b5efe5aa79b0a17db7667f0d5
Swave <maximeruff@yahoo.com>
2023-05-18 10:44:58 +1200

    Fixed vehicle issue with new models

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -5,20 +5,7 @@ public class Vehicle : MobileObject
 {
     public new void Start()
     {
-        //Setting up vehicle type first
-        if (this.vehicleType == VehicleType.Bus) //If vehicle is Bus
-        {
-            collider = GetComponent<BoxCollider>();
-            sportiness = controller.random.Next(10, 50) / 100f;
-            ((BoxCollider) collider).size = new Vector3(1.5f, 1.5f, 1.5f);
-        }
-        else //We assume it is a car
-        {
-            collider = GetComponent<SphereCollider>();
-            sportiness = controller.random.Next(50, 100) / 100f;
-            ((SphereCollider) collider).radius = 1.5f;
-        }
-        
+        collider = GetComponent<SphereCollider>();
         base.Start();
     }
 
@@ -34,12 +21,14 @@ public class Vehicle : MobileObject
             obj = Instantiate(controller.vehicleModel);
             vehicle = obj.AddComponent<Vehicle>();
             vehicle.vehicleType = VehicleType.Car;
+            vehicle.sportiness = controller.random.Next(50, 100) / 100f;
         }
         else //We want to make a bus
         {
             obj = Instantiate(controller.busModel);
             vehicle = obj.AddComponent<Vehicle>();
             vehicle.vehicleType = VehicleType.Bus;
+            vehicle.sportiness = controller.random.Next(10, 50) / 100f;
         }
 
         obj.layer = LayerMask.NameToLayer("MobileObject");
@@ -58,6 +47,8 @@ public class Vehicle : MobileObject
         vehicle.id = id;
         vehicle.controller = controller;
         controller.vehicles.Add(vehicle);
+        SphereCollider collider = obj.AddComponent<SphereCollider>();
+        collider.radius = 1.5f;
 
         return obj;
     }
