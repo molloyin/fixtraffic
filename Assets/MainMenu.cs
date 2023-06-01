3c408a94afc07fae18e3e8fe59ac730b2cdaedae
Max Somerville <max.somerville62@gmail.com>
2023-06-01 22:23:37 +1200

    Main Menu - Start button
    Start button is implemented. Unloads the main menu scene and then loads in a scene based on a set string in the MainMenu.cs file

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private const string gameScene = "building_map"; //Variable for game scene
+
     /**<summary>
      * Is used to quit the game.
      * </summary>
@@ -14,4 +17,18 @@ public class MainMenu : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false; //For in editor
     }
 
+
+    /**<summary>
+     * Loads in the game scene and unloads the main menu scene
+     * </summary>
+     */
+    public void startGame()
+    {
+        //Unloading main menu scene
+        SceneManager.UnloadSceneAsync("Main_Menu");
+
+        //Loading in game scene
+        SceneManager.LoadScene(gameScene);
+
+    }
 }
